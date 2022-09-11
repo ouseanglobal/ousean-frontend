a0d7a18969be4c9a1c0f0560198526ea3c808e8f
Muhammad Iqbal <iqbal.adudu@gmail.com>
2022-09-11 21:21:44 +0700

    Add linter

---
+++ pages/_app.jsx
@@ -0,0 +1,14 @@
+import '../styles/globals.css';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function MyApp({ Component, pageProps }) {
+  return (<Component {...pageProps} />);
+}
+
+MyApp.propTypes = {
+  Component: PropTypes.node.isRequired,
+  pageProps: PropTypes.shape({}).isRequired,
+};
+
+export default MyApp;
