285abf1321d3036d900a685bd3d42517ac95591f
Muhammad Iqbal <iqbal.adudu@gmail.com>
2022-09-11 21:34:18 +0700

    test pre commit

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css';
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 function MyApp({ Component, pageProps }) {
   return (<Component {...pageProps} />);
