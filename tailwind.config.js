62af8e806e15ed413c94dc5d14c364dbf7058672
Alpinsil <alvin1001@gmail.com>
2022-09-17 11:55:35 +0700

    selesai membuat header, hero, alasan dan event components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./src/components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                Montserrat: ["Montserrat"],
+                Poppins: ["Poppins"],
+            },
+        },
+    },
+    plugins: [],
 };
