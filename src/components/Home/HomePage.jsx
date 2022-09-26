824dc7630a37961f5fd6d33b756f335ae32fa658
Alpinsil <alvin1001@gmail.com>
2022-09-26 21:06:10 +0700

    finished

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import Layout from '../Layout/Layout';
-import Alasan from './Alasan/Alasan';
-import Cta from './Cta/Cta';
-import Event from './Event/Event';
-import Hero from './Hero/Hero';
-import Testimoni from './Testimoni/Testimoni';
+// import Alasan from './Alasan/Alasan';
+// import Cta from './Cta/Cta';
+// import Event from './Event/Event';
+// import Hero from './Hero/Hero';
+// import Testimoni from './Testimoni/Testimoni';
+// import Career from './Career/Career';
+import Hero from '../Hero/Hero';
+import Visimis from '../VisiMisi/Visimisi';
 
 function HomePage() {
   return (
     <Layout>
       <Hero />
-      <Alasan />
-      <Event />
-      <Testimoni />
-      <Cta />
+      <Visimis />
     </Layout>
   );
 }
