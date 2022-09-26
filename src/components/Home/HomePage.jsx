3c71175e72a6187e47372d384808bad286f5fc22
juandi <juandi.syahputra.tsm1@gmail.com>
2022-09-26 21:47:46 +0700

    menambahkan folder partner

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -5,7 +5,7 @@ import Cta from './Cta/Cta';
 import Event from './Event/Event';
 import Hero from './Hero/Hero';
 import Testimoni from './Testimoni/Testimoni';
-import Partner from './Partner';
+import Partner from './Partner/Partner';
 
 function HomePage() {
   return (
