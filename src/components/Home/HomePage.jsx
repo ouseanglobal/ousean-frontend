7abef9730b060bf4e792f98e231428a99914aa97
Alpinsil <alvin1001@gmail.com>
2022-09-27 06:52:37 +0700

    fixing hero

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import Layout from '../Layout/Layout';
-// import Alasan from './Alasan/Alasan';
-// import Cta from './Cta/Cta';
-// import Event from './Event/Event';
-// import Hero from './Hero/Hero';
-// import Testimoni from './Testimoni/Testimoni';
+import Hero from './Hero/Hero';
+import Alasan from './Alasan/Alasan';
+import Event from './Event/Event';
+import Cta from './Cta/Cta';
+import Testimoni from './Testimoni/Testimoni';
 // import Career from './Career/Career';
-import Hero from '../Hero/Hero';
-import Visimis from '../VisiMisi/Visimisi';
+// import Her from '../Hero/Hero';
+// import Visimis from '../VisiMisi/Visimisi';
 
 function HomePage() {
   return (
     <Layout>
       <Hero />
-      <Visimis />
+      <Alasan />
+      <Event />
+      <Cta />
+      <Testimoni />
     </Layout>
   );
 }
