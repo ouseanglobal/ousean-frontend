3b7c400b72d22a9acce9d9dc84851719478c1d58
Muhammad Iqbal <iqbal.adudu@gmail.com>
2022-10-14 19:09:36 +0700

    hotfix

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Footer from './Footer/Footer';
 import Header from './Header/Header';
+
 export default function Layout({ children }) {
   return (
     <>
